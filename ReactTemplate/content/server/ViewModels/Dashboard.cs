677491afbcfe378012b5079de0ebabb46a0613b3
Dicky Suryadi <dsuryd@gmail.com>
2017-12-07 19:19:04 -0800

    Refactor to use reactive, dynamic properties.

--- ReactTemplate/content/server/ViewModels/Dashboard.cs
+++ ReactTemplate/content/server/ViewModels/Dashboard.cs
@@ -2,16 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
+using DotNetify;
 using DotNetify.Routing;
 using DotNetify.Security;
 
 namespace dotnetify_react_template
 {
    [Authorize]
-   public class Dashboard : DotNetify.BaseVM, IRoutable
+   public class Dashboard : BaseVM, IRoutable
    {
-      private readonly IDisposable[] _subscriptions;
-      private readonly object _lock = new object();
+      private IDisposable _subscription;
 
       public class Activity
       {
@@ -22,108 +22,46 @@ namespace dotnetify_react_template
 
       public RoutingState RoutingState { get; set; }
 
-      public string Download
-      {
-         get => Get<string>();
-         set => Set(value);
-      }
-
-      public string Upload
-      {
-         get => Get<string>();
-         set => Set(value);
-      }
-
-      public string Latency
-      {
-         get => Get<string>();
-         set => Set(value);
-      }
-
-      public int Users
-      {
-         get => Get<int>();
-         set => Set(value);
-      }
-
-      public int[] Traffic
-      {
-         get => Get<int[]>();
-         set => Set(value);
-      }
-
-      public int[] ServerUsage
-      {
-         get => Get<int[]>();
-         set => Set(value);
-      }
-
       public string[] ServerUsageLabel => new string[] { "dns", "sql", "nethst", "w2k", "ubnt", "uat", "ftp", "smtp", "exch", "demo" };
-
-      public Activity[] RecentActivities
-      {
-         get => Get<Activity[]>();
-         set => Set(value);
-      }
-
-      public int[] Utilization
-      {
-         get => Get<int[]>();
-         set => Set(value);
-      }
-
       public string[] UtilizationLabel => new string[] { "Memory", "Disk", "Network" };
 
       public Dashboard(ILiveDataService liveDataService)
       {
-         _subscriptions = new IDisposable[]
+         AddProperty<string>("Download").SubscribeTo(liveDataService.Download);
+         AddProperty<string>("Upload").SubscribeTo(liveDataService.Upload);
+         AddProperty<string>("Latency").SubscribeTo(liveDataService.Latency);
+         AddProperty<int>("Users").SubscribeTo(liveDataService.Users);
+         AddProperty<int[]>("Traffic").SubscribeTo(liveDataService.Traffic);
+         AddProperty<int[]>("ServerUsage").SubscribeTo(liveDataService.ServerUsage);
+         AddProperty<int[]>("Utilization").SubscribeTo(liveDataService.Utilization);
+
+         AddProperty<Activity[]>("RecentActivities").SubscribeTo(liveDataService.RecentActivity.Select(value =>
          {
-            liveDataService.Download.Subscribe(value => SafeUpdate(() => Download = value)),
-            liveDataService.Upload.Subscribe(value => SafeUpdate(() => Upload = value)),
-            liveDataService.Latency.Subscribe(value => SafeUpdate(() => Latency = value)),
-            liveDataService.Users.Subscribe(value => SafeUpdate(() => Users = value)),
-            liveDataService.Traffic.Subscribe(value => SafeUpdate(() => Traffic = value)),
-            liveDataService.ServerUsage.Subscribe(value => SafeUpdate(() => ServerUsage = value)),
-            liveDataService.Utilization.Subscribe(value => SafeUpdate(() => Utilization = value)),
-            liveDataService.RecentActivity.Subscribe(value =>
+            var activities = new Queue<Activity>(Get<Activity[]>("RecentActivities")?.Reverse() ?? new Activity[] { });
+            activities.Enqueue(new Activity
             {
-               var activities = new Queue<Activity>(RecentActivities?.Reverse() ?? new Activity[] { });
-               activities.Enqueue(new Activity
-               {
-                  PersonName = value.PersonName,
-                  Status = value.Status,
-                  Route = this.Redirect(AppLayout.FormPagePath, value.Id.ToString())
-               });
+               PersonName = value.PersonName,
+               Status = value.Status,
+               Route = this.Redirect(AppLayout.FormPagePath, value.Id.ToString())
+            });
 
-               if (activities.Count > 4)
-                  activities.Dequeue();
+            if (activities.Count > 4)
+               activities.Dequeue();
 
-               SafeUpdate(() => RecentActivities = activities.Reverse().ToArray());
-            }),
+            return activities.Reverse().ToArray();
+         }));
 
-            // Regulate data update interval to no less than every 200 msecs.
-            Observable
-               .Interval(TimeSpan.FromMilliseconds(200))
-               .StartWith(0)
-               .Subscribe(_ =>
-               {
-                  lock(_lock)
-                  {
-                     if (ChangedProperties.Any())
-                        PushUpdates();
-                  }
-               })
-         };
+         // Regulate data update interval to no less than every 200 msecs.
+         _subscription = Observable
+            .Interval(TimeSpan.FromMilliseconds(200))
+            .StartWith(0)
+            .Subscribe(_ => PushUpdates());
       }
 
-      public override void Dispose() => _subscriptions.ToList().ForEach(i => i.Dispose());
-
-      private void SafeUpdate(Action action)
+      public override void Dispose()
       {
-         lock (_lock)
-         {
-            action.Invoke();
-         }
+         _subscription?.Dispose();
+         base.Dispose();
       }
    }
 }
