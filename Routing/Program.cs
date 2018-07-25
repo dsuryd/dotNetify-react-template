f5165058e1c05dd622988c3801e3fba059886d0c
Dicky Suryadi <dsuryd@gmail.com>
2018-07-24 19:12:06 -0700

    Update package version.

--- Routing/Program.cs
+++ Routing/Program.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Routing
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+         CreateWebHostBuilder(args).Build().Run();
+      }
 
-            host.Run();
-        }
-    }
+      public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+          WebHost.CreateDefaultBuilder(args)
+              .UseStartup<Startup>();
+   }
 }
